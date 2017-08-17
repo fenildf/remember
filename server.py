7dc2a9ff94f383537b0fd62b20a1c291be52d8f8
wangyupeng <wangyupeng129@126.com>
2017-08-17 23:31:17 +0800

    网站初版

--- server.py
+++ server.py
@@ -2,11 +2,12 @@
 # -*- coding:utf-8 -*-
 
 # all the imports
-import sqlite3
+import json
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+    abort, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 import random
+from sqlalchemy import and_
 
 # configuration
 SQLALCHEMY_DATABASE_URI = 'sqlite:///./databases.db'
@@ -20,13 +21,13 @@ app.config.from_object(__name__)
 
 db = SQLAlchemy(app)
 
+
 class EnglishWords(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     word = db.Column('word', db.String(50))
     term_frequency = db.Column('term_frequency', db.Integer)
     images = db.Column('images', db.Text)
     ext = db.Column('ext', db.Text)
-
 
     def __init__(self, word, term_frequency, images, ext):
         self.word = word
@@ -40,21 +41,52 @@ class EnglishWords(db.Model):
 
 @app.route('/')
 def show_index():
-    min_word = EnglishWords.query.filter(EnglishWords.term_frequency > 50).order_by(EnglishWords.id).first()
-    max_word = EnglishWords.query.filter(EnglishWords.term_frequency > 50).order_by(EnglishWords.id.desc()).first()
+    min_word = EnglishWords.query.filter(EnglishWords.term_frequency > 100).order_by(EnglishWords.id).first()
+    max_word = EnglishWords.query.filter(EnglishWords.term_frequency > 100).order_by(EnglishWords.id.desc()).first()
 
     random_id = random.randint(min_word.id, max_word.id)
 
     word = EnglishWords.query.filter(EnglishWords.id >= random_id).first()
 
-    return render_template('show_index.html', entries=word)
+    ext = json.loads(word.ext)
+    images = json.loads(word.images)
+
+    if not ext['translation']:
+        db.session.delete(word)
+        db.session.commit()
+        print(word)
+
+    word.images = images
+    word.ext = ext
+
+    data = {}
+    data['word'] = word
+
+    return render_template('show_index.html', data=data)
+
 
 @app.route('/tongji')
 def show_contji():
-    count = EnglishWords.query.filter(EnglishWords.term_frequency > 50).count()
+    count = []
+    categories = []
 
-    return render_template('tongji.html')
+    for i in range(1, 10):
+        s = i * 10 - 9
+        e = i * 10
+
+        categories.append('-'.join([str(s), str(e)]))
+
+        item_count = EnglishWords.query.filter(
+            and_(EnglishWords.term_frequency >= s, EnglishWords.term_frequency < e)).count()
+        count.append(item_count)
+
+    item_count = EnglishWords.query.filter(EnglishWords.term_frequency >= 100).count()
+    count.append(item_count)
+    categories.append('>100')
+
+    return render_template('tongji.html', count=count, categories=categories)
 
 
 if __name__ == '__main__':
-    app.run()
+    # show_index()
+    app.run(host='0.0.0.0')
